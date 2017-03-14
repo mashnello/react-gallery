88c449ae99357d635e56f3ad2cb020e07a43d3c3
mashnello <mashnenko@ukr.net>
2017-03-14 20:18:05 +0200

    update images

--- src/services/data.js
+++ src/services/data.js
@@ -30,33 +30,33 @@ let data = {
         "tooltip": "Lorem ipsum dolor sit."
     },
     "6987155412357": {
-        "src": "/src/images/luggage.jpg",
-        "name": "Luggage",
+        "src": "/src/images/antique.jpg",
+        "name": "Antique",
         "tooltip": "Lorem ipsum."
     },
     "6987155412358": {
-        "src": "/src/images/sofa.jpg",
-        "name": "Sofa",
+        "src": "/src/images/clown.jpg",
+        "name": "Clown",
         "tooltip": "Lorem ipsum dolor sit amet."
     },
     "9871554123459": {
-        "src": "/src/images/kermit.jpg",
-        "name": "Kermit",
+        "src": "/src/images/fur.jpg",
+        "name": "Fur",
         "tooltip": "Lorem ipsum dolor sit."
     },
     "9871554123460": {
-        "src": "/src/images/yawn.jpg",
-        "name": "Yawning",
+        "src": "/src/images/sad.jpg",
+        "name": "Sad",
         "tooltip": "Lorem ipsum dolor sit."
     },
     "9871554123461": {
-        "src": "/src/images/teddy.jpg",
-        "name": "Teddy",
+        "src": "/src/images/shopping.jpg",
+        "name": "Shopping",
         "tooltip": "Lorem ipsum dolor sit."
     },
     "9871554123462": {
-        "src": "/src/images/alone.jpg",
-        "name": "Alone",
+        "src": "/src/images/sweet.jpg",
+        "name": "Sweet",
         "tooltip": "Lorem ipsum dolor sit."
     }
 };
